e541af7fdba2cb2f6d2469c7a1226611adbca406
Luis Isaac <luiscubiche@gmail.com>
2023-03-09 20:34:56 -0500

    cloudinary and bugs

--- src/pages/Library.js
+++ src/pages/Library.js
@@ -1,33 +1,32 @@
 import { LoadingContext } from "../context/loading.context";
+import { AuthContext } from "../context/auth.context";
 import { useState, useEffect } from "react";
-import { useContext } from 'react'
+import { useContext } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import { baseUrl } from "../services/baseUrl";
+import { post } from "../services/authService";
 
 const Library = () => {
-  const {
-    user,
-    setDetails,
-    setUser,
+  const { user, setDetails, setUser } = useContext(LoadingContext);
+  const { authenticateUser } = useContext(AuthContext);
+  const [review, setReview] = useState("");
 
-  } = useContext(LoadingContext);
-
-const [review,setReview] = useState('');
-
-
-  { console.log('Wish in library', user) }
-
+  {
+    console.log("Wish in library", user);
+  }
 
   useEffect(() => {
+    // authenticateUser();
+  }, [user]);
 
-  }, [user])
-
+  console.log(user);
   // ${baseUrl}/games/delete/add-wish/${user._id}
 
   const handleAddToWishlistDelete = (user, game) => {
+    console.log(game);
     axios
-      .post(`${baseUrl}/games/delete/add-wish/${user._id}`, { gameId: game.id })
+      .post(`${baseUrl}/games/delete/add-wish/${user._id}`, { gameId: game })
       .then((response) => {
         console.log("Game deleted", response.data);
         setUser(response.data);
@@ -36,39 +35,51 @@ const [review,setReview] = useState('');
         console.log(error.message);
       });
   };
+  // console.log(
+  //   user.games_pick.map((game) => {
+  //     console.log(game);
+  //   })
+  // );
 
-const handleComment = (e) => {
-  e.preventDefault()
-}
+  const handleCommentSubmit = (gameId) => {
+    // user.games_pick.forEach((game) => {
+    //   if (game.id === gameId) {
+    post(`/games/reviews/${gameId}/${user._id}`, { review })
+      .then((results) => {
+        console.log(results.data);
+        setUser(results.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
 
-
+    // });
+  };
+  console.log(user);
   return (
-
-
-
-
     <div>
       <h1>My List</h1>
       <div className="parentLib">
-
         {user ? (
           <>
-
-            <h1> {console.log('Wish in library inside', user)}</h1>
+            {/* <h1> {console.log("Wish in library inside", user)}</h1> */}
 
             {user.games_pick.map((game) => (
               <div className="game-card-container-lib" key={game.id}>
                 <div className="game-card-lib">
-                  <Link onClick={() => setDetails(game)} to={`/games/${game.id}`}>
-                    <img src={game.background_image} alt="Gameimg" />
-                  </Link>
+                  {/* <Link
+                    onClick={() => setDetails(game)}
+                    to={`/games/${game.id}`}
+                  > */}
+                  <img src={game.background_image} alt="Gameimg" />
+                  {/* </Link> */}
                   <h2>{game.name}</h2>
                   <ul>
-                    {game.genres.length && (
+                    {/* {game.genres.length && (
                       <li>
                         <strong>Genre </strong> {game.genres[0].name}
                       </li>
-                    )}
+                    )} */}
                     <li>
                       <strong>Rating </strong>
                       {game.rating}
@@ -83,26 +94,43 @@ const handleComment = (e) => {
                       {game.released}
                     </li>
                   </ul>
-                  <label>
-                    Comment:
-                    <textarea name="review" value={review} onChange={event => setReview(event.target.value)} />
-                  </label>
+                  user: {user.name}
+                  comment: {game.review && game.review.comment}
+
+                  <form
+                    onSubmit={(e) => {
+                      e.preventDefault();
+                      console.log(game._id);
+                      handleCommentSubmit(game._id);
+                    }}
+                  >
+                    <label>
+                      Comment:
+                      <textarea
+                        name="review"
+                        onChange={(event) => setReview(event.target.value)}
+                      />
+                    </label>
+                    <button>comment</button>
+                  </form>
                 </div>
                 <div>
-                  <button className="check-button" onClick={() => handleAddToWishlistDelete(user, game)}><a className="check-button-a">Finish</a></button>
+                  <button
+                    className="check-button"
+                    onClick={() => handleAddToWishlistDelete(user, game.id)}
+                  >
+                    <a className="check-button-a">Finish</a>
+                  </button>
                 </div>
               </div>
             ))}
-
           </>
         ) : (
           <p>No games found in your wishlist.</p>
         )}
       </div>
     </div>
-
   );
 };
 
-
-export default Library
+export default Library; 
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,119 +1,150 @@
 import { useState, useEffect, useContext } from "react";
 import { LoadingContext } from "../context/loading.context";
+
 import axios from "axios";
 import { baseUrl } from "../services/baseUrl";
+import { post } from "../services/authService";
 
 const Profile = () => {
-    const { user, setUser, editing, setEditing } = useContext(LoadingContext);
+  const { user, setUser, editing, setEditing } = useContext(LoadingContext);
 
-    const [profile, setProfile] = useState({
-        profile_image: "",
-        bio: "",
-        age: "",
-    });
+  const [profile, setProfile] = useState({
+    profile_image: "",
+    bio: "",
+    age: "",
+  });
 
-    useEffect(() => {
-        // Fetch the user's profile data
-        if (user) {
-            axios
-                .get(`${baseUrl}/users/profile/${user._id}`)
-                .then((res) => {
-                    console.log('profile res', res.data.profile)
-                    setProfile(res.data.profile);
-                })
-                .catch((err) => {
-                    console.log(err);
-                });
-        }
-    }, [user]);
+  const [imageUrl, setImageUrl] = useState("");
 
-    const handleEdit = (e) => {
-        e.preventDefault();
-        setEditing(true);
-    };
 
-    const handleSave = (e) => {
-        e.preventDefault();
-        // Save the profile data
-        axios
-            .post(`${baseUrl}/users/profile-edit/${user._id}`, profile)
-            .then((res) => {
-                setUser((prevUser) => ({
-                    ...prevUser,
-                    profile: res.data.profile,
-                }));
-                setEditing(false);
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-    };
+  useEffect(() => {
+    // Fetch the user's profile data
+    if (user) {
+      axios
+        .get(`${baseUrl}/users/profile/${user._id}`)
+        .then((results) => {
+          console.log(results.data, "my profile");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }, [user]);
 
-    const handleCancel = (e) => {
-        e.preventDefault();
+  const handleEdit = (e) => {
+    e.preventDefault();
+    setEditing(true);
+  };
+
+  const handleSave = (e) => {
+    e.preventDefault();
+    // Save the profile data
+    axios
+      .post(`${baseUrl}/users/profile-edit/${user._id}`, profile)
+      .then((res) => {
+        setUser(res.data);
+        // setUser((prevUser) => ({
+        //   ...prevUser,
+        //   profile: res.data.profile,
+        // }));
         setEditing(false);
-    };
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-    const handleInputChange = (e) => {
+  const handleCancel = (e) => {
+    e.preventDefault();
+    setEditing(false);
+  };
+
+  const handleInputChange = (e) => {
+    setProfile({
+      ...profile,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleFileSubmit = (e) => {
+    let fileUpload = new FormData()
+    fileUpload.append("profile_image", e.target.files[0])
+    post('/users/upload', fileUpload)
+    .then((result) =>{
+        console.log(result)
         setProfile({
             ...profile,
-            [e.target.name]: e.target.value,
+            profile_image: result.data,
+            
         });
-    };
+        console.log(profile)
+    })
+    .catch((err) => {
+     console.log(err)
+    })
+ }
 
-    return (
-        <div>
+
+
+  return (
+    <div>
+      <div>
+        {editing ? (
+          <form encType="multipart/form-data" onSubmit={handleSave}>
             <div>
-                {editing ? (
-                    <form onSubmit={handleSave}>
-                        <div>
-                            <label >Profile Image</label>
-                            <input
-                                type="text"
-                                name="profile_image"
-                                value={profile.profile_image || ""}
-                                onChange={handleInputChange}
-                            />
-                        </div>
-                        <div>
-                            <label >Bio</label>
-                            <textarea
-                                name="bio"
-                                value={profile.bio}
-                                onChange={handleInputChange}
-                            ></textarea>
-                        </div>
-                        <div>
-                            <label >Age</label>
-                            <input
-                                type="text"
-                                name="age"
-                                value={profile.age}
-                                onChange={handleInputChange}
-                            />
-                        </div>
-                        <button type="submit">Save</button>
-                        <button onClick={handleCancel}>Cancel</button>
-                    </form>
-                ) : (
-                    <div>
-                        <div>
-                            <img src={profile.profile_image} alt="Profile" />
-                        </div>
-                        <div>
-                            <h2>Bio</h2>
-                            <p>{profile.bio}</p>
-                        </div>
-                        <div>
-                            <h2>Age</h2>
-                            <p>{profile.age}</p>
-                        </div>
-                        <button onClick={handleEdit}>Edit Profile</button>
-                    </div>
-                )}
+            <label htmlFor="profile_image">Image:</label>
+            <input
+              type="file"
+              id="profile_image"
+              name="profile_image"
+              className="form-control-file"
+              onChange={handleFileSubmit}
+             
+            />
             </div>
-        </div>
-    );
+            <div>
+              <label>Bio</label>
+              <textarea
+                name="bio"
+                value={profile.bio}
+                onChange={handleInputChange}
+              ></textarea>
+            </div>
+            <div>
+              <label>Age</label>
+              <input
+                type="text"
+                name="age"
+                value={profile.age}
+                onChange={handleInputChange}
+              />
+            </div>
+            <button type="submit">Save</button>
+            <button onClick={handleCancel}>Cancel</button>
+          </form>
+        ) : (
+          <div>
+            <div>
+              <img
+                src={user ? user.profile_image : <p>no image</p>}
+                alt="Profile"
+                style={{ width: "150px" }}
+              />
+            </div>
+            <div>
+              <h2>Bio</h2>
+              {user ? user.bio : <p>no bio</p>}
+            </div>
+            <div>
+              <h2>Age</h2>
+              {user ? user.age : <p>no age</p>}
+            </div>
+            <button onClick={handleEdit}>Edit Profile</button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
-export default Profile;
+export default Profile; 
